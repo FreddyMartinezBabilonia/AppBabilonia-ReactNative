519fa4fc9ff7d32f6e1f8f214e84c000b15d21b3
codigoinerte <fmartinez.bpe@gmail.com>
2024-05-17 18:07:08 -0500

    feat: interceptar de url _blank y abrir en la webview

--- src/screens/main/home.tsx
+++ src/screens/main/home.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import WebView from 'react-native-webview'
 
 export const Home = () => {
+  const [uri, setUri] = useState("https://www-testing.babilonia.pe/");
   return (
     <WebView 
-        source={{ uri: 'https://babilonia.pe/' }} 
+        source={{ uri }} 
         cacheEnabled={true}
+        onOpenWindow={(syntheticEvent) => {
+          const { nativeEvent } = syntheticEvent;
+          const { targetUrl } = nativeEvent
+          setUri(targetUrl);
+          //console.log('Intercepted OpenWindow for', targetUrl)
+        }}
         style={{ flex: 1 }} />
   )
 }

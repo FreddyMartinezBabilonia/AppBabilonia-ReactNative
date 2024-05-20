40bffbd086d40d2e9b336d4d3a99f9aa5ab8eb50
codigoinerte <fmartinez.bpe@gmail.com>
2024-05-19 21:17:11 -0500

    feat: se habilito el boton de retroceso en la vista home

--- src/screens/main/home.tsx
+++ src/screens/main/home.tsx
@@ -1,22 +1,42 @@
-import React, { useState } from 'react'
-import { Linking } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react'
+import { BackHandler, Linking } from 'react-native';
 import WebView from 'react-native-webview';
 import SplashScreen from 'react-native-splash-screen';
 
 export const Home = () => {
   const domain = "www-testing.babilonia.pe";
   const [uri, setUri] = useState(`https://${domain}/`);
-
+  const webViewRef = useRef<any>(null);
+  const [canGoBack, setCanGoBack] = useState(false);
+  
   const handleWebViewLoad = () => {
     // Ocultar el splash screen cuando el WebView haya cargado
     SplashScreen.hide();
   };
 
+
+
+  // Manejar el evento de retroceso
+  const handleBackPress = () => {
+    if (canGoBack) {
+      webViewRef!.current!.goBack();
+      return true; // Previene que el comportamiento por defecto (salir de la app) ocurra
+    }
+    return false; // Permite que el comportamiento por defecto ocurra (salir de la app)
+  };
+
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', handleBackPress);
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', handleBackPress);
+    };
+  }, [canGoBack]);
+
   return (
     <WebView
-      
+      ref={webViewRef}
       source={{uri}}
-      cacheEnabled={true}
+      cacheEnabled={false}
       onLoad={handleWebViewLoad}
       onOpenWindow={syntheticEvent => {
         const {nativeEvent} = syntheticEvent;
@@ -27,7 +47,9 @@ export const Home = () => {
           isTrue:targetUrl.includes(domain) && !targetUrl.includes("wa.me")
         });
         */
+       console.log(targetUrl);
         if (targetUrl.includes(domain) && !targetUrl.includes("wa.me")){
+          console.log('open url');
           setUri(targetUrl);
         }else{
           Linking.openURL(targetUrl);
@@ -36,6 +58,7 @@ export const Home = () => {
       }}
       
       style={{flex: 1}}
+      onNavigationStateChange={(navState) => setCanGoBack(navState.canGoBack)}
     />
   );
 }

4d752020715429ef4611c81c147f4793f9afcc83
Fredy Martinez <fredy.martinez@gmail.com>
2024-05-20 18:49:17 -0500

    feat: instalacion de requerida para react native config

--- android/app/src/main/java/com/babilonia/MainApplication.kt
+++ android/app/src/main/java/com/babilonia/MainApplication.kt
@@ -12,6 +12,7 @@ import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.lugg.RNCConfig.RNCConfigPackage;
 
 class MainApplication : Application(), ReactApplication {
 

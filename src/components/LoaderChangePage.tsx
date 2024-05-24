f63ac875d701385ce6b6a5ba12b31d5ed7f90f6e
codigoinerte <fmartinez.bpe@gmail.com>
2024-05-24 12:52:51 -0500

    fix: se cambio loader dinamico por loader lottie

--- src/components/LoaderChangePage.tsx
+++ src/components/LoaderChangePage.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import { ActivityIndicator, StyleSheet, View } from 'react-native'
+import React, { useEffect, useRef } from 'react'
+import { StyleSheet, View } from 'react-native';
+import LottieView from "lottie-react-native";
 
 export const LoaderChangePage = () => {
+  const animationRef = useRef<LottieView>(null);
+  useEffect(() => {
+    animationRef.current?.play();
+  }, []);
+
   return (
     <>
         <View style={styles.loader}>
-            <ActivityIndicator color={"#fff"} size={50} />
+          <LottieView
+            ref={animationRef}
+            source={require("../resources/json/loader.json")}
+            style={styles.animateView}
+          />
         </View>
     </>
   )
@@ -13,9 +23,14 @@ export const LoaderChangePage = () => {
 
 const styles = StyleSheet.create({
     loader: {
-      backgroundColor: "#083766",
+      backgroundColor: "#ffffff",
       flex:1,
       alignItems:"center",
       justifyContent:"center"
+    },
+    animateView : {
+      width: "100%",
+      height: "100%",
+      flex: 1
     }
 });

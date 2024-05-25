b9e047e89169d323c69203e5b9f3d17af8fdf968
codigoinerte <fmartinez.bpe@gmail.com>
2024-05-24 21:27:17 -0500

    feat: creacion de navigator para manejo de screens

---
+++ src/navigator/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Home } from '../screens';
+const Stack = createStackNavigator();
+
+export const Navigator = () => {
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{ headerShown:false }}>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}

4726111c31b9404f60626cfa39f7ccb3b5c82abf
codigoinerte <fmartinez.bpe@gmail.com>
2024-05-24 21:26:58 -0500

    feat: implementación de use Effect para detectar deeplinks

--- src/hooks/useHome.tsx
+++ src/hooks/useHome.tsx
@@ -4,7 +4,7 @@ import { getEnviroment, openPlayStore } from '../helpers';
 import { ModalNativeProps } from '../interfaces';
 import { api } from '../api';
 import SplashScreen from 'react-native-splash-screen';
-import { AppState, BackHandler } from 'react-native';
+import { AppState, BackHandler, Linking } from 'react-native';
 import DeviceInfo from 'react-native-device-info';
 
 export const useHome = () => {
@@ -16,32 +16,46 @@ export const useHome = () => {
         });
     `;
     const domain = getEnviroment("API_BASE_WEB");
-    const uri = `https://${domain}/`;
-
+    
     const webViewRef = useRef<any>(null);
+    const [uri, setUri] = useState(`https://${domain}/`)
     const [update, setUpdate] = useState(false);
     const [canGoBack, setCanGoBack] = useState(false);
     const [loader, setLoader] = useState(false);
     const [appState, setAppState] = useState(AppState.currentState);
 
+    const _handleOpenURL = (url:any) => {
+        if(!url) return;
+        setUri(url);
+    }
+
     useEffect(() => {
-        BackHandler.addEventListener('hardwareBackPress', handleBackPress);
-        return () => {
-          BackHandler.removeEventListener('hardwareBackPress', handleBackPress);
-        };
-      }, [canGoBack]);
+      Linking.getInitialURL().then((e) => {
+        _handleOpenURL(e);
+      }).catch(err => {
+          console.warn('An error occurred', err);
+      });
+      Linking.addEventListener('url', ({ url }) =>{ _handleOpenURL(url) });
+    }, []);
     
-      useEffect(() => {
-        const subscription = AppState.addEventListener('change', nextAppState => {
-          if(appState.match(/inactive|background/) && nextAppState === 'active') {
-            if(update == false) launchAndroidAlert();
-          }
-          setAppState(nextAppState);
-        });
-        return () => {
-          subscription.remove();
-        };
-      }, [appState]);
+    useEffect(() => {
+      BackHandler.addEventListener('hardwareBackPress', handleBackPress);
+      return () => {
+        BackHandler.removeEventListener('hardwareBackPress', handleBackPress);
+      };
+    }, [canGoBack]);
+    
+    useEffect(() => {
+      const subscription = AppState.addEventListener('change', nextAppState => {
+        if(appState.match(/inactive|background/) && nextAppState === 'active') {
+          if(update == false) launchAndroidAlert();
+        }
+        setAppState(nextAppState);
+      });
+      return () => {
+        subscription.remove();
+      };
+    }, [appState]);
 
     const launchAndroidAlert = () => {
         const props:ModalNativeProps = {

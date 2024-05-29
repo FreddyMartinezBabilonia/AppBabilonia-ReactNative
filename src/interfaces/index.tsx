eebda0bdf471db96322ffe9811bb2d3f7e563c6b
codigoinerte <fmartinez.bpe@gmail.com>
2024-05-29 12:10:23 -0500

    feat: se agrego interface MessageResponse, DownloadParams y DownloadFileParams

--- src/interfaces/index.tsx
+++ src/interfaces/index.tsx
@@ -3,3 +3,19 @@ export interface ModalNativeProps {
     message: string;
     buttons: { text: string; onPress: () => void }[];
 }
+export interface MessageResponse {
+    bearer?: string;
+    url?:    string;
+    type?:   'listings' | 'interested.detail.listings' | 'interested.detail.project' | 'interested.home' | 'collections';
+}
+
+export interface DownloadParams {
+    bearer:    string;
+    url:       string;
+}
+
+export interface DownloadFileParams extends DownloadParams{
+    fileName:   string;
+    type:       string;
+    api?:        string;
+}

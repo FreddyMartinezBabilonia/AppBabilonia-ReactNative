6de393ef61679e05bd73bb039f08c4b61ba8dd1a
codigoinerte <fmartinez.bpe@gmail.com>
2024-06-01 01:01:11 -0500

    feat: implementacion de card/Icon...

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,3 +2,8 @@ export * from './ModalNative';
 export * from './LoaderChangePage';
 export * from './BottomSheetCustom';
 export * from './Button';
+export * from './CustomIcon';
+
+export * from './card/IconTypeProperty';
+export * from './card/IconFavorite';
+export * from './card/IconCamera';
